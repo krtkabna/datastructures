87a77705f4f6ebb7f19ff0b195449f8f7cc3e306
wasp <wasp@rocke.tech>
2022-01-20 11:10:43 +0200

    implement LinkedList methods

--- src/main/java/com/wasp/datastructures/list/LinkedList.java
+++ src/main/java/com/wasp/datastructures/list/LinkedList.java
@@ -1,10 +1,14 @@
 package com.wasp.datastructures.list;
 
+import java.util.Objects;
+
 public class LinkedList implements List {
+    private static final String INDEX_OOB_MSG = "Index out of bounds: ";
     private Node head;
     private int size;
 
-    public LinkedList() {}
+    public LinkedList() {
+    }
 
     @Override
     public void add(Object value) {
@@ -18,11 +22,14 @@ public class LinkedList implements List {
             }
             tail.next = newNode;
         }
+        size++;
     }
 
     @Override
     public void add(Object value, int index) {
-        checkIndexOOB(index);
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException(INDEX_OOB_MSG + index);
+        }
         Node temp = new Node(value);
         Node current = head;
         for (int i = 0; i < index - 1; i++) {
@@ -36,23 +43,34 @@ public class LinkedList implements List {
 
     @Override
     public Object remove(int index) {
-        Node temp = head;
-        for (int i = 0; i < index - 1; i++) {
-            temp = temp.next;
+        checkIndexOOB(index);
+        Node curr = head;
+        Object result;
+        if (index == 0) {
+            result = curr.data;
+            curr.next = head;
+        } else {
+            Node prev = head;
+            for (int i = 1; i < index; i++) {
+                prev = curr;
+                curr = curr.next;
+            }
+            result = curr.data;
+            prev.next = curr.next.next;
         }
-        temp.next = temp.next.next;
 
         size--;
-        return null;
+        return result;
     }
 
     @Override
     public Object get(int index) {
+        checkIndexOOB(index);
         Node temp = head;
-        for (int i = 0; i < index - 1; i++) {
+        for (int i = 0; i < index; i++) {
             temp = temp.next;
         }
-        return null;
+        return temp.data;
     }
 
     @Override
@@ -60,15 +78,16 @@ public class LinkedList implements List {
         checkIndexOOB(index);
         Node toSet = new Node(value);
         Node temp = head;
-        for (int i = 0; i < index - 1; i++) {
+        for (int i = 0; i < index; i++) {
             temp = temp.next;
         }
-        toSet.next = temp.next;
-        return temp;
+        temp.data = toSet.data;
+        return temp.data;
     }
 
     @Override
     public void clear() {
+        head = null;
         size = 0;
     }
 
@@ -79,26 +98,52 @@ public class LinkedList implements List {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
     }
 
     @Override
     public boolean contains(Object value) {
-        return false;
+        return indexOf(value) != -1;
     }
 
     @Override
     public int indexOf(Object value) {
-        return 0;
+        Node temp = head;
+        for (int i = 0; i < size(); i++) {
+            if (Objects.equals(temp.data, value)) {
+                return i;
+            }
+            temp = temp.next;
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Object value) {
-        return 0;
+        Node temp = head;
+        for (int i = size() - 1; i >= 0; i--) {
+            if (Objects.equals(temp.data, value)) {
+                return i;
+            }
+            temp = temp.next;
+        }
+        return -1;
     }
 
     public String toString() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Node temp = head;
+        for (int i = 0; i < size(); i++) {
+            sb.append(temp.data.toString());
+            if (i == size() - 1) {
+                sb.append("]");
+            } else {
+                sb.append(", ");
+            }
+            temp = temp.next;
+        }
+        return sb.toString();
     }
 
     private void checkIndexOOB(int index) {
